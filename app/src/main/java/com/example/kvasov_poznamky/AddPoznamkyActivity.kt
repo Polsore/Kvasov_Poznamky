4cda650ba907dc93f94d71d71c40ede5f045e7e6
Dmytro Kvasov <dimka.ups02@gmail.com>
2022-06-14 13:08:49 +0300

    v1

---
+++ app/src/main/java/com/example/kvasov_poznamky/AddPoznamkyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kvasov_poznamky
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddPoznamkyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_poznamka)
+    }
+}
--- app/src/main/java/com/example/kvasov_poznamky/MainActivity.kt
+++ app/src/main/java/com/example/kvasov_poznamky/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.kvasov_poznamky
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun pridajPoznamku(view: View) {
+        val intent = Intent(this, AddPoznamkyActivity::class.java)
+        startActivity(intent)
     }
 }
